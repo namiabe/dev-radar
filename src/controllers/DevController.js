230b5e756677a53e5a8277f5d0c814d58444d7f7
Nádia Nami Abe <nadia.nami.abe@avanade.com>
2020-03-05 15:34:47 -0300

    add trycatch

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -5,40 +5,47 @@ const parseStringAsArray = require('./../utils/parseStringAsArray')
 module.exports = {
 
     async index (req, res) {
-        const devs = await Dev.find()
-        return res.json(devs)
+        try {
+            const devs = await Dev.find()
+            return res.status(200).json(devs)
+        } catch (error) {
+            res.status(400).json({ message: error.message})
+        }
     },
 
     async store (req, res) {
-        const { github_username, techs, latitude, longitude } = req.body 
-
-        let dev = await Dev.findOne({ github_username })
-
-        if(!dev) {
-
-            const response = await axios.get(`https://api.github.com/users/${github_username}`)
-        
-            const {  name = login, avatar_url, bio } = response.data
-        
-            const techsArray = parseStringAsArray(techs)
-        
-            const location = {
-                type: 'Point',
-                coordinates: [longitude, latitude]
+        try {
+            const { github_username, techs, latitude, longitude } = req.body 
+    
+            let dev = await Dev.findOne({ github_username })
+    
+            if(!dev) {
+    
+                const response = await axios.get(`https://api.github.com/users/${github_username}`)
+            
+                const {  name = login, avatar_url, bio } = response.data
+            
+                const techsArray = parseStringAsArray(techs)
+            
+                const location = {
+                    type: 'Point',
+                    coordinates: [longitude, latitude]
+                }
+            
+                dev = await Dev.create({
+                    github_username,
+                    name,
+                    avatar_url,
+                    bio,
+                    techs: techsArray,
+                    location
+                })
             }
-        
-            dev = await Dev.create({
-                github_username,
-                name,
-                avatar_url,
-                bio,
-                techs: techsArray,
-                location
-            })
+    
+            return res.status(200).json(dev)
+        } catch (error) {
+            res.status(400).json({ message: error.message})
         }
-
-        return res.json(dev)
     }
-
 
 }
